0cb6962e11aebf32599723b6539541884ed91457
luisa <luisa@192.168.1.2>
2021-02-18 15:30:37 +0100

    cambio dsprova in profilo in servlet

--- TSW/src/control/ProfiloUtenteServlet.java
+++ TSW/src/control/ProfiloUtenteServlet.java
@@ -134,7 +134,7 @@ public class ProfiloUtenteServlet extends HttpServlet {
 				
 				utenteModel.doUpdate(utente);
 				request.getSession().setAttribute("utenteSessione", utente);
-				RequestDispatcher view = request.getRequestDispatcher("dsprova.jsp");
+				RequestDispatcher view = request.getRequestDispatcher("profilo.jsp");
 				view.forward(request,response);
 				if(utenteModel.doUpdate(utente)==1) {
 					System.out.println("risultato 1");
